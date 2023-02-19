22c932877674c4cbcba4c715d9ad20d7c0f21da5
Achim Gädke <achimgaedke@users.noreply.github.com>
2023-02-20 12:01:48 +1300

    CLI: add criterion/path arguments, add tests

--- src/pyprojroot2/cli.py
+++ src/pyprojroot2/cli.py
@@ -2,31 +2,73 @@
 # usable, e.g. as `pyprojroot` returning the default root
 
 import argparse
+import pathlib
 import sys
+import typing
 
 from . import __version__
-from .predefined_criteria import py_here_criteria
+from .root_criterion import RootCriterion
+from . import predefined_criteria
+
+DEFAULT_CRITERION = "py_here_criteria"
 
 
-def main() -> int:
+def get_criterion(name: str) -> RootCriterion:
+    criterion = getattr(predefined_criteria, name)
+    assert isinstance(criterion, RootCriterion)
+    return criterion
+
+
+def list_criteria_names() -> typing.List[str]:
+    # todo: check with "__all__"
+    # todo: return a dict with criteria
+    # todo: move this function to the predefined_criteria module
+    all_criteria_names = [
+        name
+        for name in dir(predefined_criteria)
+        if isinstance(getattr(predefined_criteria, name), RootCriterion)
+    ]
+    assert DEFAULT_CRITERION in all_criteria_names
+    return all_criteria_names
+
+
+def make_argument_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
         description="Determines the root directory of a project (e.g. git)",
         epilog="""
-Based on pyprojroot2, this command searches for a project root signature in the parent
-directories starting from the current directory. When successful it prints out the
-absolute pathname of the project root, otherwise exits with an error (1) and a message.
+This command searches for the project root starting with the current directory
+and iterating through the parent directories. When successfully matching the
+criterion, it prints out the absolute pathname of the project root.
+Otherwise prints a message on STDERR and exits with an error (1).
 """,
     )
+
     parser.add_argument(
         "--version", action="version", version=f"%(prog)s {__version__}"
     )
-    parser.parse_args()
 
-    # this should be specifically "--here"
-    # other predefined roots (instances of RootCriterion) should be selectable
-    # with --criterion xxx
+    # add criterion argument
+    parser.add_argument(
+        "-c",
+        "--criterion",
+        type=str,
+        default=DEFAULT_CRITERION,
+        choices=list_criteria_names(),
+        help="project root criterion",
+    )
+
+    # add start path positional argument
+    parser.add_argument("path", type=pathlib.Path, nargs="?", default=".")
+    return parser
+
+
+def cli_main() -> int:
+    parser = make_argument_parser()
+    parse_result = parser.parse_args()
+    criterion = get_criterion(parse_result.criterion)
+
     try:
-        print(py_here_criteria.find_root())
+        print(criterion.find_root(path=parse_result.path))
         return 0
     except FileNotFoundError:
         print("could not find the project root", file=sys.stderr)
---
+++ tests/test_cli.py
@@ -0,0 +1,77 @@
+import pathlib
+import sys
+
+import pytest
+
+from pyprojroot2 import cli, RootCriterion
+
+
+def test_criteria_access() -> None:
+    assert cli.list_criteria_names()
+    assert isinstance(cli.get_criterion(cli.DEFAULT_CRITERION), RootCriterion)
+
+
+def test_argparse() -> None:
+    parser = cli.make_argument_parser()
+
+    default_result = parser.parse_args([])
+    assert default_result.criterion == cli.DEFAULT_CRITERION
+    assert default_result.path == pathlib.Path(".")
+
+    criterion_result = parser.parse_args(["-c", "is_git_root"])
+    assert criterion_result.criterion == "is_git_root"
+    assert criterion_result.path == pathlib.Path(".")
+
+    with pytest.raises(SystemExit):
+        parser.parse_args(["-c", "is_dir"])
+
+    # only one criterion, the last one specified
+    criterion_result = parser.parse_args(
+        ["-c", "is_dvc_root", "--criterion", "is_git_root"]
+    )
+    assert criterion_result.criterion == "is_git_root"
+
+    path_result = parser.parse_args(["--criterion", "is_git_root", "bla"])
+    assert path_result.criterion == "is_git_root"
+    assert path_result.path == pathlib.Path("bla")
+
+
+def test_cli(
+    monkeypatch: pytest.MonkeyPatch,
+    tmp_path: pathlib.Path,
+    capsys: pytest.CaptureFixture[str],
+) -> None:
+    entrypoint_name = "pyprojroot2"
+
+    monkeypatch.chdir(tmp_path)
+
+    monkeypatch.setattr(sys, "argv", [entrypoint_name, "--criterion", "is_git_root"])
+    assert cli.cli_main() == 1
+    output = capsys.readouterr()
+    assert not output.out
+    assert output.err
+
+    # mock a git repo, but not a dvc repo
+    (tmp_path / "project/.git").mkdir(parents=True)
+
+    monkeypatch.setattr(sys, "argv", [entrypoint_name, "--criterion", "is_git_root"])
+    assert cli.cli_main() == 1
+    output = capsys.readouterr()
+    assert not output.out
+    assert output.err
+
+    monkeypatch.setattr(
+        sys, "argv", [entrypoint_name, "--criterion", "is_git_root", "project"]
+    )
+    assert cli.cli_main() == 0
+    output = capsys.readouterr()
+    assert pathlib.Path(output.out.rstrip()) == tmp_path / "project"
+    assert not output.err
+
+    monkeypatch.setattr(
+        sys, "argv", [entrypoint_name, "--criterion", "is_dvc_root", "project"]
+    )
+    assert cli.cli_main() == 1
+    output = capsys.readouterr()
+    assert not output.out
+    assert output.err
