92cc2169b74a36caabe59ea512d39ad5a05b0c26
Achim Gädke <achimgaedke@users.noreply.github.com>
2023-02-20 12:00:19 +1300

    predefined roots: clean up module

--- src/pyprojroot2/predefined_criteria.py
+++ src/pyprojroot2/predefined_criteria.py
@@ -10,16 +10,21 @@ from .root_criterion import (
     as_root_criterion,
 )
 
+# todo: __all__
+
+# functions to make criteria defined by rprojroot
 has_file_pattern = HasFilePattern
-
-has_file = HasFile
-
-has_dir = HasDir
-
 has_basename = HasBasename
 
+# rprojroot and pyprojroot 0.3.0
+has_file = HasFile
+has_dir = HasDir
+
+# only pyprojroot 0.3.0
 # https://github.com/chendaniely/pyprojroot/blob/dev/src/pyprojroot/criterion.py#L66
 matches_glob = HasEntryGlob
+
+# criteria from R project rprojroot
 
 # https://github.com/r-lib/rprojroot/blob/main/R/root.R#L309
 is_rstudio_project = has_file_pattern("[.]Rproj$", contents="^Version: ", n=1)
@@ -49,11 +54,10 @@ is_vcs_root = is_git_root | is_svn_root
 # that the first tested directory is the current directory.
 from_wd = IsCwd()
 
-# https://github.com/r-lib/here/blob/970dd2726c5cddda4a0e44e910fe5290be063485/R/set_here.R#L46
-is_here = has_file(".here")
-
-# TODO: uses subdir
+# TODO: use subdir
 # is_testthat = has_basename("testthat", c("tests/testthat", "testthat"))
+# or try... but doesn't reeturn the subdir...
+# is_testthat = has_dir("tests/testthat") | has_dir("testthat") | has_basename("testthat")
 is_testthat = has_basename("testthat")
 
 
@@ -74,6 +78,11 @@ r_criteria = as_root_criterion(
     }
 )
 
+# criteria from R project here
+
+# https://github.com/r-lib/here/blob/970dd2726c5cddda4a0e44e910fe5290be063485/R/set_here.R#L46
+is_here = has_file(".here")
+
 # https://github.com/r-lib/here/blob/970dd2726c5cddda4a0e44e910fe5290be063485/R/zzz.R#L3
 r_here_criteria = AnyCriteria(
     is_here
@@ -84,13 +93,15 @@ r_here_criteria = AnyCriteria(
     | is_vcs_root
 )
 
+# criteria provided by pyprojroot 0.2.0 and 0.3.0
+
 # see https://github.com/achimgaedke/pyprojroot2/blob/towards_v2/src/pyprojroot2/here.py#L17
 # these are a more python centric root criteria
 py_here_criteria_0_3_0 = as_root_criterion(
     [
-        HasFile(".here"),  # difference to 0.2
+        HasFile(".here"),  # difference to 0.2.0
         HasDir(".git"),
-        matches_glob("*.Rproj"),
+        HasEntryGlob("*.Rproj"),
         HasFile("requirements.txt"),
         HasFile("setup.py"),
         HasDir(".dvc"),
@@ -104,9 +115,9 @@ py_here_criteria_0_3_0 = as_root_criterion(
 # https://github.com/chendaniely/pyprojroot/blob/master/pyprojroot/pyprojroot.py#L23
 py_here_criteria_0_2_0 = as_root_criterion(
     [
-        HasEntry(".git"),  # difference to 0.3
+        HasEntry(".git"),  # difference to 0.3.0
         HasEntry(".here"),
-        matches_glob("*.Rproj"),
+        HasEntryGlob("*.Rproj"),
         HasEntry("requirements.txt"),
         HasEntry("setup.py"),
         HasEntry(".dvc"),
@@ -117,5 +128,5 @@ py_here_criteria_0_2_0 = as_root_criterion(
     ]
 )
 
-# the defaults for this project
+# the default root criterion for this project
 py_here_criteria = py_here_criteria_0_2_0
